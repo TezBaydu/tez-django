17d3f0b725ccdbad5c879abd258d65092da737e1
TezBaydu <tezbaydu@gmail.com>
2021-10-16 12:12:18 +0000

    setup development environment

--- todo/views.py
+++ todo/views.py
@@ -15,8 +15,8 @@ def add_item(request):
     if request.method == "POST":
         form = ItemForm(request.POST)
         if form.is_valid():
-           form.save()
-           return redirect('get_todo_list')
+            form.save()
+            return redirect('get_todo_list')
     form = ItemForm()
     context = {
         'form': form
@@ -29,8 +29,8 @@ def edit_item(request, item_id):
     if request.method == "POST":
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
-           form.save()
-           return redirect('get_todo_list')
+            form.save()
+            return redirect('get_todo_list')
     form = ItemForm(instance=item)
     context = {
         'form': form
